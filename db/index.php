315fe13c450ef9ed73bac29bb69f6573ec3273f1
Andreas Goetz <cpuidle@gmx.de>
2015-08-16 11:40:25 +0200

    Restructure /db command

--- db/index.php
+++ db/index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3, or (at your option)
@@ -18,202 +18,188 @@
  *	Tsunamp Team
  *	http://www.tsunamp.com
  *
- *	UI-design/JS code by: 	Andrea Coiutti (aka ACX)
- *	PHP/JS code by:			Simone De Gregori (aka Orion)
- * 
- *	file:					db/index.php
- * 	version:				1.0
- *
- *	TCMODS Edition 
- *
- * 	TC (Tim Curtis) 2014-09-17, r1.0
- *	- added case 'savedplaylist': to enumerate contents of saved playlist  
- *
- * 	TC (Tim Curtis) 2014-12-23, r1.3
- *	- added case 'deleteradiostn': to handle radio station delete
- *	- added case 'addradiostn': to handle radio station add and update
- *	- added case "trackmove' to handle moving playlist tracks
- *	- debug Library addAll $_POST not matching what was sent, turned out to be php.ini max_input_vars = 1000 limit
- *	- shovel & broom
- *
+ * Rewrite by Tim Curtis and Andreas Goetz
  */
 
-// common include
-include('../inc/connection.php');
-error_reporting(ERRORLEVEL);
+require_once dirname(__FILE__) . '/../inc/connection.php';
 
-if (isset($_GET['cmd']) && $_GET['cmd'] != '') {
-    if ( !$mpd ) {
-       echo 'Error connecting to MPD server';
-	} else {
-		switch ($_GET['cmd']) {
-			case 'filepath':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					echo json_encode(searchDB($mpd,'filepath',$_POST['path']));
-				} else {
-					echo json_encode(searchDB($mpd,'filepath'));
-				}
-				break;
-				
-			// TC (Tim Curtis) 2014-11-30
-			// - delete radio station file
-			case 'deleteradiostn':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					sysCmd("rm \""."/var/lib/mpd/music/".$_POST['path']."\"");
-					// update time stamp on files so MPD picks up the change and commits the update
-					sysCmd("find /var/lib/mpd/music/WEBRADIO -name \""."*.pls"."\""." -exec touch {} \+");
-				}
-				break;
-				
-			// TC (Tim Curtis) 2014-12-23
-			// - add radio station file, also used for update
-			case 'addradiostn':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					// create new file if none exists, or open existing file for overwrite 
-					$_file = '/var/lib/mpd/music/WEBRADIO/'.$_POST['path'].'.pls';
-					$handle = fopen($_file, 'w') or die('db/index.php: file create failed on '.$_file);
-					// format .pls lines
-					$data = '[playlist]'."\n";
-					$data .= 'numberofentries=1'."\n";
-					$data .= 'File1='.$_POST['url']."\n";
-					$data .= 'Title1='.$_POST['path']."\n";
-					$data .= 'Length1=-1'."\n";
-					$data .= 'version=2'."\n";
-					// write data, close file
-					fwrite($handle, $data);
-					fclose($handle);
-					// reset file permissions
-					sysCmd("chmod 777 \"".$_file."\"");
-					// update time stamp on files so MPD picks up the change and commits the update
-					sysCmd("find /var/lib/mpd/music/WEBRADIO -name \""."*.pls"."\""." -exec touch {} \+");
-				}
-				break;
-				
-			// TC (Tim Curtis) 2014-09-17
-			// - list contents of saved playlist
-			// - delete saved playlist
-			case 'listsavedpl':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					echo json_encode(listPlayList($mpd, $_POST['path']));
-				}
-				break;
-				
-			case 'deletesavedpl':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					echo json_encode(removePlayList($mpd, $_POST['path']));
-				}
-				break;
-				
-			case 'playlist':
-				echo json_encode(getPlayQueue($mpd));
-				break;
-				
-			case 'add':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					echo json_encode(addQueue($mpd,$_POST['path']));
-				}
-				break;
-				
-			case 'addplay':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					$status = _parseStatusResponse(MpdStatus($mpd));
-					$pos = $status['playlistlength'] ;
-					addQueue($mpd,$_POST['path']);
-					sendMpdCommand($mpd,'play '.$pos);
-					echo json_encode(readMpdResponse($mpd));
-				}
-				break;
-				
-			case 'addreplaceplay':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					sendMpdCommand($mpd,'clear');
-					addQueue($mpd,$_POST['path']);
-					sendMpdCommand($mpd,'play');
-					echo json_encode(readMpdResponse($mpd));
-				}
-				break;
-				
-			case 'update':
-				if (isset($_POST['path']) && $_POST['path'] != '') {
-					sendMpdCommand($mpd,"update \"".html_entity_decode($_POST['path'])."\"");
-					echo json_encode(readMpdResponse($mpd));
-				}
-				break;
-				
-			case 'trackremove':
-				if (isset($_GET['songid']) && $_GET['songid'] != '') {
-					echo json_encode(remTrackQueue($mpd,$_GET['songid']));
-				}
-				break;
-			
-			// TC (Tim Curtis) 2014-12-23
-			// - move playlist tracks	
-			case 'trackmove':
-				if (isset($_GET['songid']) && $_GET['songid'] != '') {
-					$_args = $_GET['songid'].' '.$_GET['newpos'];
-					sendMpdCommand($mpd,'move '.$_args);
-					echo json_encode('track move args= '.$_args);
-				}
-				break;
-
-            case 'savepl':
-                if (isset($_GET['plname']) && $_GET['plname'] != '') {
-                    sendMpdCommand($mpd,"rm \"".html_entity_decode($_GET['plname'])."\"");
-                    sendMpdCommand($mpd,"save \"".html_entity_decode($_GET['plname'])."\"");
-                    echo json_encode(readMpdResponse($mpd));
-                }
-				break;
-				
-			case 'search':
-				if (isset($_POST['query']) && $_POST['query'] != '' && isset($_GET['querytype']) && $_GET['querytype'] != '') {
-					echo json_encode(searchDB($mpd,$_GET['querytype'],$_POST['query']));
-				}
-				break;
-				
-            case 'loadlib':
-				echo loadAllLib($mpd);
-            	break;
-            	
-            case 'addall':
-                if (isset($_POST['path']) && $_POST['path'] != '') {
-                    echo json_encode(enqueueAll($mpd,$_POST['path']));
-				}
-				break;
-				
-            // TC (Tim Curtis) 2014-09-17	
-            // - added code to set the playlist song pos for play	
-            case 'playall':
-                if (isset($_POST['path']) && $_POST['path'] != '') {
-					// TC just a copy/paste from addplay above 
-					$status = _parseStatusResponse(MpdStatus($mpd));
-					$pos = $status['playlistlength'] ;
-                	// original code, did not set play posn
-                	echo json_encode(playAll($mpd,$_POST['path']));
-					sendMpdCommand($mpd,'play '.$pos);
-					echo json_encode(readMpdResponse($mpd));
-                }
-				break;
-				
-			// TC (Tim Curtis) 2014-09-17
-			// - library panel Add/replace/playall btn
-            case 'addallreplaceplay':
-                if (isset($_POST['path']) && $_POST['path'] != '') {
-	               
-	               // TC (Tim Curtis) 2014-12-23
-	               // - to debug $_POST not matching what was sent
-	               // - turned out to be php.ini max_input_vars = 1000 limit 
-	               //echo json_encode($_POST['path']);
-                   echo json_encode(playAllReplace($mpd,$_POST['path']));
-				}
-				break;
-			} // end switch
-			
-		closeMpdSocket($mpd);
-	}
-} else {
-	echo 'MPD DB INTERFACE<br>';
-	echo 'INTERNAL USE ONLY<br>';
-	echo 'hosted on raspyfi.local:81';
+function mpdTouchFiles() {
+	return sysCmd("find '" . MPD_LIB . 'WEBRADIO' . "' -name \"" . "*.pls" . "\"" . " -exec touch {} \+");
 }
-?>
 
+if (!$mpd) {
+	die('Error: connection to MPD failed');
+}
+
+if (!isset($_GET['cmd'])) {
+	die('Error: missing or invalid command');
+}
+
+$cmd = $_GET['cmd'];
+$path = isset($_POST['path']) ? $_POST['path'] : null;
+
+$res = array('OK' => true);
+
+
+switch ($cmd) {
+	case 'filepath':
+		$res = (null !== $path)
+			? searchDB($mpd, 'filepath', $path)
+			: searchDB($mpd, 'filepath');
+		break;
+
+	// - delete radio station file
+	case 'deleteradiostn':
+		if (null !== $path) {
+			$res = array('syscmd' => array());
+			$res['syscmd'][] = sysCmd("rm '" . MPD_LIB . $path . "'");
+			// update time stamp on files so MPD picks up the change and commits the update
+			$res['syscmd'][] = mpdTouchFiles();
+		}
+		break;
+
+	// - add radio station file, also used for update
+	case 'addradiostn':
+		if (null !== $path) {
+			$res = array('syscmd' => array());
+
+			// create new file if none exists, or open existing file for overwrite
+			$_file = MPD_LIB . 'WEBRADIO/' . $path . '.pls';
+			$handle = fopen($_file, 'w') or die('db/index.php: file create failed on '.$_file);
+			// format .pls lines
+			$data = '[playlist]' . "\n";
+			$data .= 'numberofentries=1' . "\n";
+			$data .= 'File1='.$_POST['url'] . "\n";
+			$data .= 'Title1=' . $path . "\n";
+			$data .= 'Length1=-1' . "\n";
+			$data .= 'version=2' . "\n";
+			// write data, close file
+			fwrite($handle, $data);
+			fclose($handle);
+
+			// reset file permissions
+			$res['syscmd'][] = sysCmd("chmod 777 \"" .$_file . "\"");
+			// update time stamp on files so MPD picks up the change and commits the update
+			$res['syscmd'][] = mpdTouchFiles();
+		}
+		break;
+
+	// - list contents of saved playlist
+	// - delete saved playlist
+	case 'listsavedpl':
+		if (null !== $path) {
+			$res = listPlayList($mpd, $path);
+		}
+		break;
+
+	case 'deletesavedpl':
+		if (null !== $path) {
+			$res = removePlayList($mpd, $path);
+		}
+		break;
+
+	case 'playlist':
+		$res = getPlayQueue($mpd);
+		break;
+
+	case 'add':
+		if (null !== $path) {
+			$res = addQueue($mpd, $path);
+		}
+		break;
+
+	case 'addplay':
+		if (null !== $path) {
+			$status = _parseStatusResponse(MpdStatus($mpd));
+			$pos = $status['playlistlength'] ;
+			addQueue($mpd, $path);
+			sendMpdCommand($mpd, 'play '.$pos);
+			$res = readMpdResponse($mpd);
+		}
+		break;
+
+	case 'addreplaceplay':
+		if (null !== $path) {
+			sendMpdCommand($mpd, 'clear');
+			addQueue($mpd, $path);
+			sendMpdCommand($mpd, 'play');
+			$res = readMpdResponse($mpd);
+		}
+		break;
+
+	case 'update':
+		if (null !== $path) {
+			sendMpdCommand($mpd,"update \"" .html_entity_decode($path) . "\"");
+			$res = readMpdResponse($mpd);
+		}
+		break;
+
+	case 'trackremove':
+		if (isset($_GET['songid']) && $_GET['songid'] != '') {
+			$res = remTrackQueue($mpd,$_GET['songid']);
+		}
+		break;
+
+	// TC (Tim Curtis) 2014-12-23
+	// - move playlist tracks
+	case 'trackmove':
+		if (isset($_GET['songid']) && $_GET['songid'] != '') {
+			$_args = $_GET['songid'].' '.$_GET['newpos'];
+			sendMpdCommand($mpd, 'move '.$_args);
+			$res = 'track move args= '.$_args;
+		}
+		break;
+
+	case 'savepl':
+		if (isset($_GET['plname']) && $_GET['plname'] != '') {
+			sendMpdCommand($mpd,"rm \"" .html_entity_decode($_GET['plname']) . "\"");
+			sendMpdCommand($mpd,"save \"" .html_entity_decode($_GET['plname']) . "\"");
+			$res = readMpdResponse($mpd);
+		}
+		break;
+
+	case 'search':
+		if (isset($_POST['query']) && $_POST['query'] != '' &&
+			isset($_GET['querytype']) && $_GET['querytype'] != '')
+		{
+			$res = searchDB($mpd,$_GET['querytype'],$_POST['query']);
+		}
+		break;
+
+	case 'loadlib':
+		$res = loadAllLib($mpd);
+		break;
+
+	case 'addall':
+		if (null !== $path) {
+			$res = enqueueAll($mpd, $path);
+		}
+		break;
+
+	// TC (Tim Curtis) 2014-09-17
+	// - added code to set the playlist song pos for play
+	case 'playall':
+		if (null !== $path) {
+			// TC just a copy/paste from addplay above
+			$status = _parseStatusResponse(MpdStatus($mpd));
+			$pos = $status['playlistlength'] ;
+			// original code, did not set play posn
+			$res = playAll($mpd, $path);
+			sendMpdCommand($mpd, 'play '.$pos);
+			$res = readMpdResponse($mpd);
+		}
+		break;
+
+	// TC (Tim Curtis) 2014-09-17
+	// - library panel Add/replace/playall btn
+	case 'addallreplaceplay':
+		if (null !== $path) {
+			$res = playAllReplace($mpd, $path);
+		}
+		break;
+}
+
+closeMpdSocket($mpd);
+
+header('Content-type: application/json');
+echo json_encode($res);
